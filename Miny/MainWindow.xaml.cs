b6e39426a6272c8dbb8fbfd864e8c3b89eedf5fd
JVancata <vancata.kuba@seznam.cz>
2018-10-11 09:31:16 +0200

    Recursivity! Yay!

--- Miny/MainWindow.xaml.cs
+++ Miny/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -28,28 +29,43 @@ namespace Miny
         public MainWindow()
         {
             InitializeComponent();
+            Announce.Foreground = Brushes.Black;
+            Announce.Content = "Game has started!";
+            Board.Children.Clear();
             generateGrid(cols, rows);
-
             generatePlayGround();
-
             renderGrid();
-
-
+            Board.ShowGridLines = true;
+        }
+        void startGame(int _cols, int _rows)
+        {
+            Announce.Foreground = Brushes.Black;
+            Announce.Content = "Game has started!";
+            playGround = new int[10, 10];
+            Board.Children.Clear();
+            generateGrid(cols, rows);
+            generatePlayGround();
+            renderGrid();
             Board.ShowGridLines = true;
         }
         void generatePlayGround()
         {
-            int bombs = difficulty * 10;
-
-            playGround[0, 0] = 10;
-            playGround[0, 5] = 10;
-            playGround[0, 4] = 10;
-            playGround[0, 3] = 10;
-            playGround[9, 2] = 10;
-            /*while (bombs>0)
-            {
-
-            }*/
+            int bombs = difficulty * 20;
+            Random rand = new Random();
+            while (bombs > 0)
+            {                
+                for (int _row = 0; _row < rows; _row++)
+                {
+                    for (int _col = 0; _col < cols; _col++)
+                    {
+                        if(rand.Next(0, 40) == 10)
+                        {
+                            playGround[_col, _row] = 9;
+                            bombs--;
+                        }
+                    }
+                }
+            }
         }
         void renderGrid()
         {
@@ -69,22 +85,33 @@ namespace Miny
                     {
                         button.Content = "X";
                     }
+                    //FLAG
+                    if (playGround[_col, _row] == 11)
+                    {
+                        button.Content = "F";
+                        button.Background = Brushes.Red;
+                    }
                     //EMPTY
                     if (playGround[_col, _row] == 10)
                     {
                         button.Content = "";
+                        button.Background = Brushes.LightGray;
                     }
                     //BOMB NEAR
                     else if (playGround[_col, _row] > 0 && playGround[_col, _row] < 9)
                     {
                         button.Content = playGround[_col, _row].ToString();
+                        button.Background = Brushes.LightGray;
                     }
-                    //THIS IS NEODHALENO
-                    else
+                    
+                    //THIS IS NEODHALENO = 0
+                    else if (playGround[_col, _row] == 0)
                     {
-                        button.Content = "Nevim";
+                        button.Content = "";
+                        button.Background = Brushes.DarkGray;
                     }
-                    button.Click += clickButton;
+                    button.Click += clickButtonLeft;
+                    button.MouseRightButtonUp += clickButtonRight;
                     Grid.SetRow(button, _row);
                     Grid.SetColumn(button, _col);
                     Board.Children.Add(button);
@@ -97,17 +124,17 @@ namespace Miny
             int bombCounter = 0;
 
             // LEFT
-            if (_col > 1)
+            if (_col > 0)
             {
                 if (playGround[_col - 1, _row] == 9) bombCounter++;
             }
             // LEFT DIAGONAL UP
-            if (_col > 1 && _row > 1)
+            if (_col > 0 && _row > 0)
             {
                 if (playGround[_col - 1, _row - 1] == 9) bombCounter++;
             }
             // UP
-            if (_row > 1)
+            if (_row > 0)
             {
                 if (playGround[_col, _row - 1] == 9) bombCounter++;
             }
@@ -117,7 +144,7 @@ namespace Miny
                 if (playGround[_col + 1, _row] == 9) bombCounter++;
             }
             // RIGHT DIAGONAL UP
-            if (_col < cols - 1 && _row > 1)
+            if (_col < cols - 1 && _row > 0)
             {
                 if (playGround[_col + 1, _row - 1] == 9) bombCounter++;
             }
@@ -127,7 +154,7 @@ namespace Miny
                 if (playGround[_col, _row + 1] == 9) bombCounter++;
             }
             // LEFT DIAGONAL DOWN
-            if (_col > 1 && _row < rows - 1)
+            if (_col > 0 && _row < rows - 1)
             {
                 if (playGround[_col - 1, _row + 1] == 9) bombCounter++;
             }
@@ -141,30 +168,115 @@ namespace Miny
 
         }
 
-        void checkClick(int row, int col)
+        void showNotShown(int col, int row)
+        {
+
+            if (col > 0)
+            {
+                int nextCol = col - 1;
+                int nextRow = row;
+                int count = countBombs(playGround, nextCol, nextRow);
+                if (count == 0)
+                {
+                    if (playGround[nextCol, nextRow] == 0)
+                    {
+                        playGround[nextCol, nextRow] = 10;
+                        showNotShown(nextCol, nextRow);
+                    }
+                }
+                else if (playGround[nextCol, nextRow] != 9)
+                {
+                    playGround[nextCol, nextRow] = count;
+                }
+            }
+            if (col < 9)
+            {
+                int nextCol = col + 1;
+                int nextRow = row;
+                int count = countBombs(playGround, nextCol, nextRow);
+                if (count == 0)
+                {
+                    if (playGround[nextCol, nextRow] == 0)
+                    {
+                        playGround[nextCol, nextRow] = 10;
+                        showNotShown(nextCol, nextRow);
+                    }
+
+                }
+                else if (playGround[nextCol, nextRow] != 9)
+                {
+                    playGround[nextCol, nextRow] = count;
+                }
+            }
+            if (row > 0)
+            {
+                int nextCol = col;
+                int nextRow = row - 1;
+                int count = countBombs(playGround, nextCol, nextRow);
+                if (count == 0)
+                {
+                    if (playGround[nextCol, nextRow] == 0)
+                    {
+                        playGround[nextCol, nextRow] = 10;
+                        showNotShown(nextCol, nextRow);
+                    }
+                }
+                else if (playGround[nextCol, nextRow] != 9)
+                {
+                    playGround[nextCol, nextRow] = count;
+                }
+            }
+            if (row < 9)
+            {
+                int nextCol = col;
+                int nextRow = row + 1;
+                int count = countBombs(playGround, col, row + 1);
+                if (count == 0)
+                {
+                    if (playGround[nextCol, nextRow] == 0)
+                    {
+                        playGround[nextCol, nextRow] = 10;
+                        showNotShown(nextCol, nextRow);
+                    }
+                }
+                else if (playGround[nextCol, nextRow] != 9)
+                {
+                    playGround[nextCol, nextRow] = count;
+                }
+            }
+        }
+
+        void checkClick(int col, int row)
         {
             // BOMB CLICK
             if (playGround[col, row] == 9)
             {
-                // LOST GAME
-            };
+                Announce.Foreground = Brushes.Red;
+                Announce.Content = "You lost!";
+                //startGame(cols, rows);
+            }
+            // 
+            // Debug.WriteLine(countBombs(playGround, col, row));
             // NEODHALENO FIELD CLICK
-            if (playGround[col, row] == 0)
+            else if (playGround[col, row] == 0)
             {
-                int _col = col;
-                int _row = row;
-
-                while (true)
-                {
-                    // countBombs and reveal empty fields
-                }
+                int count = countBombs(playGround, col, row);
+                if (count == 0) playGround[col, row] = 10;
+                else playGround[col, row] = count;
+                showNotShown(col, row);
             };
             renderGrid();
         }
 
-        void clickButton(object sender, RoutedEventArgs e)
+        void clickButtonLeft(object sender, RoutedEventArgs e)
         {
-            checkClick(Grid.GetRow((Button)sender), Grid.GetColumn((Button)sender));
+            checkClick(Grid.GetColumn((Button)sender), Grid.GetRow((Button)sender));
+            renderGrid();
+        }
+        void clickButtonRight(object sender, MouseEventArgs e)
+        {
+            // playGround[Grid.GetColumn((Button)sender), Grid.GetRow((Button)sender)] = 11;
+            // renderGrid();
         }
 
         void generateGrid(int cols, int rows)
